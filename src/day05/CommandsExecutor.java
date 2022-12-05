1c9301ebb528447f4f63344bb109959affee8060
albert <ehrenberger_albert@yahoo.com>
2022-12-05 22:51:09 +0100

    Day05 first star

---
+++ src/day05/CommandsExecutor.java
@@ -0,0 +1,41 @@
+package day05;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class CommandsExecutor {
+
+    private CommandsExecutor() {
+
+    }
+
+    private static final int REPEAT_POSITION = 0;
+    private static final int FROM_POSITION = 1;
+    private static final int TO_POSITION = 2;
+
+    public static void executeCommands(List<ArrayDeque<Character>> stacks, List<String> inputs) {
+        for (String input: inputs) {
+            List<Integer> command = extractCommand(input);
+            for (int i = 0; i < command.get(REPEAT_POSITION); i++){
+                ArrayDeque<Character> fromStack = stacks.get(command.get(FROM_POSITION) - 1);
+                ArrayDeque<Character> toStack = stacks.get(command.get(TO_POSITION) - 1);
+                toStack.addLast(fromStack.removeLast());
+            }
+        }
+    }
+
+    private static List<Integer> extractCommand(String stringToSearch){
+        Pattern integerPattern = Pattern.compile("-?\\d+");
+        Matcher matcher = integerPattern.matcher(stringToSearch);
+
+        List<Integer> integerList = new ArrayList<>();
+        while (matcher.find()) {
+            integerList.add(Integer.valueOf(matcher.group()));
+        }
+
+        return integerList;
+    }
+}
--- src/day05/Main.java
+++ src/day05/Main.java
@@ -7,21 +7,32 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public final class Main {
 
     final static int STARTING_POSITION = 1;
     final static int DISTANCE_BETWEEN_CRATES = 4;
     final static int NUMBER_OF_STACKS = 9;
     final static int MAX_HEIGHT_OF_STACK = 8;
     final static int LENGTH_OF_CRATE_INPUTS = 35;
+    final static int FIRST_COMMAND_INPUT = 11;
 
     public static void main(String[] args) throws IOException {
         List<String> inputs = Files.readAllLines(Paths.get("./src/day05/input.txt"));
 
         final List<ArrayDeque<Character>> stacks = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_STACKS; i++) {
-            stacks.add(new ArrayDeque<>());
+            stacks.add(new ArrayDeque<>(0));
         }
         StacksBuilder.buildStacks(stacks, inputs.subList(0, MAX_HEIGHT_OF_STACK));
+        CommandsExecutor.executeCommands(stacks, inputs.subList(FIRST_COMMAND_INPUT - 1, inputs.size()));
+        System.out.println(printTopOfStacks(stacks));
     }
+
+        private static String printTopOfStacks(List<ArrayDeque<Character>> stacks){
+            StringBuilder ret = new StringBuilder();
+            for (ArrayDeque<Character> stack:stacks) {
+                ret.append(stack.getLast());
+            }
+            return ret.toString();
+        }
 }
