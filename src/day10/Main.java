390fcd6338e15898e0a53072a5d90e5e64c85879
albert <ehrenberger_albert@yahoo.com>
2022-12-10 19:44:25 +0100

    Day10 Second Star

--- src/day10/Main.java
+++ src/day10/Main.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<String> instructions = Files.readAllLines(Paths.get("./src/day10/testinput.txt"));
+        List<String> instructions = Files.readAllLines(Paths.get("./src/day10/input.txt"));
         SignalStrengthCalculator calculator = new SignalStrengthCalculator();
         HashMap<Integer, Integer> signalStrengths = calculator.calculateSignalStrength(instructions);
         Collection<Integer> values = signalStrengths.values();
@@ -19,5 +19,15 @@ public class Main {
             valuesSum += value;
         }
         System.out.println(valuesSum);
+
+        SignalStrengthCalculator graphicCalculator = new SignalStrengthCalculator();
+        char[][] image = graphicCalculator.drawImage(instructions);
+        for (char[] imageLine: image) {
+            StringBuilder drawnLine = new StringBuilder();
+            for (char pixel: imageLine) {
+                drawnLine.append(pixel);
+            }
+            System.out.println(drawnLine);
+        }
     }
 }
--- src/day10/SignalStrengthCalculator.java
+++ src/day10/SignalStrengthCalculator.java
@@ -8,15 +8,25 @@ public class SignalStrengthCalculator {
     private static final int FIRST_CYCLE = 20;
     private static final int CYCLE_STEP = 40;
     private static final int LAST_CYCLE = 220;
-    private static int registerX = 1;
-    private static int cycle = 0;
+    private static final int PIXELS_PER_ROW = 40;
+    private static final int PIXELS_ROWS = 6;
+    private int registerX = 1;
+    private int cycle = 0;
     private final HashMap<Integer, Integer> signalStrengths = new HashMap<>();
+    private final char[][] image = new char[PIXELS_ROWS][PIXELS_PER_ROW];
 
     public HashMap<Integer, Integer> calculateSignalStrength(List<String> instructions) {
         for (String instruction : instructions) {
             executeInstruction(instruction);
         }
         return signalStrengths;
+    }
+
+    public char[][] drawImage (List<String> instructions) {
+        for (String instruction : instructions) {
+            executeInstruction(instruction);
+        }
+        return image;
     }
 
     private void executeInstruction(String instruction) {
@@ -28,6 +38,13 @@ public class SignalStrengthCalculator {
     }
 
     private void incrementCycle() {
+        char newPixel;
+        if ( cycle % PIXELS_PER_ROW >= registerX - 1 && cycle % PIXELS_PER_ROW  <= registerX + 1){
+            newPixel = '#';
+        } else {
+            newPixel = '.';
+        }
+        image[cycle / PIXELS_PER_ROW][cycle % PIXELS_PER_ROW] = newPixel;
         cycle++;
         if (cycle >= FIRST_CYCLE && cycle <= LAST_CYCLE && (cycle - FIRST_CYCLE) % CYCLE_STEP == 0) {
             signalStrengths.put(cycle, cycle * registerX);
